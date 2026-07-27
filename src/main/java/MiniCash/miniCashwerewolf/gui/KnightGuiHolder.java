9226a901c3b6ea1c6317bc551db2837e1bc14459
MiniCash <tminecraft2143@gmail.com>
2026-07-27 21:42:56 +0900

    Delete src/main directory

--- src/main/java/MiniCash/miniCashwerewolf/gui/KnightGuiHolder.java
+++
@@ -1,9 +0,0 @@
-package MiniCash.miniCashwerewolf.gui;
-
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryHolder;
-
-public class KnightGuiHolder implements InventoryHolder {
-    @Override
-    public Inventory getInventory() { return null; }
-}
